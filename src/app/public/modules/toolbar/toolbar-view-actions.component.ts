8f8425a24b9b399089058258c50f4c7858b8dd3f
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2019-04-08 10:07:35 -0400

    Code review changes

--- src/app/public/modules/toolbar/toolbar-view-actions.component.ts
+++ src/app/public/modules/toolbar/toolbar-view-actions.component.ts
@@ -1,10 +1,12 @@
 import {
-    Component
-  } from '@angular/core';
+  ChangeDetectionStrategy,
+  Component
+} from '@angular/core';
 
-  @Component({
-    selector: 'sky-toolbar-view-actions',
-    templateUrl: './toolbar-view-actions.component.html',
-    styleUrls: ['./toolbar-view-actions.component.scss']
-  })
-  export class SkyToolbarViewActionsComponent { }
+@Component({
+  selector: 'sky-toolbar-view-actions',
+  templateUrl: './toolbar-view-actions.component.html',
+  styleUrls: ['./toolbar-view-actions.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SkyToolbarViewActionsComponent { }
