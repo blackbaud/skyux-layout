3d9cee75085efea66da6d87cf09dc0c3975bc166
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-01-28 14:33:58 -0500

    Fix action button focus boundaries

--- e2e/action-button.e2e-spec.ts
+++ e2e/action-button.e2e-spec.ts
@@ -3,9 +3,17 @@ import {
   SkyHostBrowser
 } from '@skyux-sdk/e2e';
 
+import {
+  by,
+  element
+} from 'protractor';
+
 describe('Action Button', () => {
-  it('should match previous action button screenshot', (done) => {
+  beforeEach(() => {
     SkyHostBrowser.get('visual/action-button');
+  });
+
+  it('should match previous action button screenshot', (done) => {
     SkyHostBrowser.setWindowBreakpoint('lg');
     expect('#screenshot-action-button').toMatchBaselineScreenshot(done, {
       screenshotName: 'action-button'
@@ -13,7 +21,6 @@ describe('Action Button', () => {
   });
 
   it('should match previous action button screenshot on small screens', (done) => {
-    SkyHostBrowser.get('visual/action-button');
     SkyHostBrowser.setWindowBreakpoint('xs');
     expect('#screenshot-action-button').toMatchBaselineScreenshot(done, {
       screenshotName: 'action-button-xs'
@@ -21,7 +28,6 @@ describe('Action Button', () => {
   });
 
   it('should match previous action button containerscreenshot', (done) => {
-    SkyHostBrowser.get('visual/action-button');
     SkyHostBrowser.setWindowBreakpoint('lg');
     expect('#screenshot-action-button-container').toMatchBaselineScreenshot(done, {
       screenshotName: 'action-button-container'
@@ -29,10 +35,18 @@ describe('Action Button', () => {
   });
 
   it('should match previous action button container screenshot on small screens', (done) => {
-    SkyHostBrowser.get('visual/action-button');
     SkyHostBrowser.setWindowBreakpoint('xs');
     expect('#screenshot-action-button-container').toMatchBaselineScreenshot(done, {
       screenshotName: 'action-button-container-xs'
+    });
+  });
+
+  it('should match previous focused action button screenshot', (done) => {
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    // Click the button to apply focus.
+    element(by.css('#screenshot-action-button .sky-action-button')).click();
+    expect('#screenshot-action-button').toMatchBaselineScreenshot(done, {
+      screenshotName: 'action-button-focused'
     });
   });
 });
