3a59ef531c88337c5c5cc014ae7d2177b1e94147
Trevor Burch <Trevor.Burch@blackbaud.com>
2020-02-04 15:54:10 -0500

    Fix issue where when focusing the parent it should not move focus. This can happen due to clicks

--- src/app/public/modules/inline-delete/inline-delete-adapter.service.ts
+++ src/app/public/modules/inline-delete/inline-delete-adapter.service.ts
@@ -35,9 +35,9 @@ export class SkyInlineDeleteAdapterService {
     this.element = element;
     this.parentEl = element.parentElement;
     this.parentElUnlistenFn = this.renderer.listen(this.parentEl, 'focusin',
-      (event: KeyboardEvent) => {
+      (event: FocusEvent) => {
         const target: any = event.target;
-        if (!this.element.contains(target)) {
+        if (!this.element.contains(target) && !this.element === target) {
           event.preventDefault();
           event.stopPropagation();
           event.stopImmediatePropagation();
@@ -98,7 +98,7 @@ export class SkyInlineDeleteAdapterService {
       return this.focussableElements;
     }
 
-    this.focussableElements = this.coreAdapterService.getFocusableChildren(document.body);
+    this.focussableElements = this.coreAdapterService.getFocusableChildren(document.body, { ignoreTabIndex: true });
 
     return this.focussableElements;
   }
