2819294f315cea79b0ec31e2e667e57ae054c7cd
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-02-20 14:34:56 -0500

    [rc] Fix exports API

--- src/app/public/index.ts
+++ src/app/public/index.ts
@@ -1,1 +1,27 @@
-export * from './modules';
+export * from './modules/action-button/action-button-permalink';
+export * from './modules/action-button/action-button.module';
+
+export * from './modules/card/card.module';
+
+export * from './modules/definition-list/definition-list.module';
+
+export * from './modules/dock/dock-item';
+export * from './modules/dock/dock-item-config';
+export * from './modules/dock/dock.module';
+export * from './modules/dock/dock.service';
+
+export * from './modules/fluid-grid/fluid-grid-gutter-size';
+export * from './modules/fluid-grid/fluid-grid.module';
+
+export * from './modules/inline-delete/inline-delete-type';
+export * from './modules/inline-delete/inline-delete.module';
+
+export * from './modules/page/page.module';
+
+export * from './modules/page-summary/page-summary.module';
+
+export * from './modules/text-expand/text-expand.module';
+
+export * from './modules/text-expand-repeater/text-expand-repeater.module';
+
+export * from './modules/toolbar/toolbar.module';
