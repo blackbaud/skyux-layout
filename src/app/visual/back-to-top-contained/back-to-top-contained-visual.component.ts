40a8d23eacad8cfe9a0439f40c2ccb2079c2df93
Alex Kingman <alex.kingman@blackbaud.me>
2020-02-28 10:11:23 -0500

    added visual tests. renamed contained visual test to scrollable parent

--- src/app/visual/back-to-top-contained/back-to-top-contained-visual.component.ts
+++
@@ -1,9 +0,0 @@
-import {
-  Component
-} from '@angular/core';
-
-@Component({
-  selector: 'back-to-top-contained-visual',
-  templateUrl: './back-to-top-contained-visual.component.html'
-})
-export class BackToTopContainedVisualComponent { }
