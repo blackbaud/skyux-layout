7573b7a4774a1b10ec15da747b4c7b2e26c49db9
Steve Brush <steve.brush@blackbaud.com>
2020-03-04 11:41:32 -0500

    [rc] Merge `master` (2020-03-02) (#86)

---
+++ e2e/back-to-top.e2e-spec.ts
@@ -0,0 +1,22 @@
+import {
+  expect,
+  SkyHostBrowser
+} from '@skyux-sdk/e2e';
+
+describe('Back to top', () => {
+  it('should match previous screenshot', (done) => {
+    SkyHostBrowser.get('visual/back-to-top');
+    SkyHostBrowser.scrollTo('p:last-child');
+    expect('#screenshot-window').toMatchBaselineScreenshot(done, {
+      screenshotName: 'infinite-scroll-back-to-top'
+    });
+  });
+
+  it('should match previous screenshot with scrollable parent element', (done) => {
+    SkyHostBrowser.get('visual/back-to-top-scrollable-parent');
+    SkyHostBrowser.scrollTo('p:last-child');
+    expect('#screenshot-window').toMatchBaselineScreenshot(done, {
+      screenshotName: 'infinite-scroll-back-to-top-scrollable-parent'
+    });
+  });
+});
