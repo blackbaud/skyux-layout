64f3e29b1a9a1c3fc4c85959b48f9bf9aeb4d7af
Denise Pena <denise.pena@blackbaud.me>
2020-06-29 17:06:35 -0500

    Added docs for Development tab

--- src/app/public/modules/toolbar/toolbar-item.component.ts
+++ src/app/public/modules/toolbar/toolbar-item.component.ts
@@ -2,6 +2,10 @@ import {
   Component
 } from '@angular/core';
 
+/**
+ * Specifies a container for an item in the toolbar.
+ * @required
+ */
 @Component({
   selector: 'sky-toolbar-item',
   styleUrls: ['./toolbar-item.component.scss'],
--- src/app/public/modules/toolbar/toolbar-view-actions.component.ts
+++ src/app/public/modules/toolbar/toolbar-view-actions.component.ts
@@ -3,6 +3,9 @@ import {
   Component
 } from '@angular/core';
 
+/**
+ * Adds a section on the right side of the toolbar for items that substantially alter the view of the content container. This includes simple filters and view switchers. For best practices on the order of toolbar items and item placement, see UX guidelines.
+ */
 @Component({
   selector: 'sky-toolbar-view-actions',
   templateUrl: './toolbar-view-actions.component.html',
