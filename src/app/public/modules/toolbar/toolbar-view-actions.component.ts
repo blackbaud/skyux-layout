67f0c91daf60ec88bb831e5fe517271360ee5a74
Denise Pena <denise.pena@blackbaud.me>
2020-06-30 12:03:28 -0500

    Fixed line length error

--- src/app/public/modules/toolbar/toolbar-view-actions.component.ts
+++ src/app/public/modules/toolbar/toolbar-view-actions.component.ts
@@ -4,7 +4,9 @@ import {
 } from '@angular/core';
 
 /**
- * Adds a section on the right side of the toolbar for items that substantially alter the view of the content container. This includes simple filters and view switchers. For best practices on the order of toolbar items and item placement, see UX guidelines.
+ * Adds a section on the right side of the toolbar for items that substantially alter
+ * the view of the content container. This includes simple filters and view switchers.
+ * For best practices on the order of toolbar items and item placement, see UX guidelines.
  */
 @Component({
   selector: 'sky-toolbar-view-actions',
