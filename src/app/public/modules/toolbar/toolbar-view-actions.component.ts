40f935b74f847bc2025af0088764d9dc3eb2d8d0
Denise Pena <denise.pena@blackbaud.me>
2020-07-17 16:26:23 -0500

    Implemented feedback

--- src/app/public/modules/toolbar/toolbar-view-actions.component.ts
+++ src/app/public/modules/toolbar/toolbar-view-actions.component.ts
@@ -6,7 +6,6 @@ import {
 /**
  * Adds a section on the right side of the toolbar for items that substantially alter
  * the view of the content container. This includes simple filters and view switchers.
- * For best practices on the order of toolbar items and item placement, see UX guidelines.
  */
 @Component({
   selector: 'sky-toolbar-view-actions',
