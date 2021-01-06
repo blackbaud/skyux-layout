7b02d818993a542f512a847a16fb27e65dc32528
Alex Kingman <alex.kingman@blackbaud.me>
2021-01-06 16:17:55 -0500

    modern theme uses sky-font-demphasized class

--- src/app/public/modules/definition-list/definition-list-value.component.ts
+++ src/app/public/modules/definition-list/definition-list-value.component.ts
@@ -1,9 +1,24 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
+  OnDestroy,
+  OnInit,
   TemplateRef,
   ViewChild
 } from '@angular/core';
+
+import {
+  SkyThemeService
+} from '@skyux/theme';
+
+import {
+  takeUntil
+} from 'rxjs/operators';
+
+import {
+  Subject
+} from 'rxjs';
 
 import {
   SkyDefinitionListService
@@ -17,9 +32,11 @@ import {
   templateUrl: './definition-list-value.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SkyDefinitionListValueComponent {
+export class SkyDefinitionListValueComponent implements OnDestroy, OnInit {
 
   public defaultValue: string;
+
+  public themeName: string;
 
   @ViewChild('valueTemplateRef', {
     read: TemplateRef,
@@ -27,8 +44,31 @@ export class SkyDefinitionListValueComponent {
   })
   public templateRef: TemplateRef<any>;
 
+  private ngUnsubscribe = new Subject();
+
   constructor(
-    public service: SkyDefinitionListService
+    public service: SkyDefinitionListService,
+    private changeRef: ChangeDetectorRef,
+    private themeSvc: SkyThemeService
   ) {}
+
+  public ngOnInit(): void {
+    if (this.themeSvc) {
+      this.themeSvc.settingsChange
+        .pipe(
+          takeUntil(this.ngUnsubscribe)
+        )
+        .subscribe((themeSettings) => {
+          this.themeName = themeSettings.currentSettings?.theme?.name;
+          console.log(this.themeName);
+          this.changeRef.markForCheck();
+        });
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 
 }
