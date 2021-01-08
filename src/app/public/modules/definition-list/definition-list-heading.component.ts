3cee9bd471674b501c4b37cdb8c9a3fdf7a50cc9
Alex Kingman <alex.kingman@blackbaud.me>
2021-01-08 09:39:02 -0500

    words

--- src/app/public/modules/definition-list/definition-list-heading.component.ts
+++ src/app/public/modules/definition-list/definition-list-heading.component.ts
@@ -6,7 +6,7 @@ import {
 /**
  * Specifies a title for the definition list.
  * @deprecated Do not use the `<sky-definition-list-heading>` component in your template
- * (this will be a breaking change in the next major version release). Instead,
+ * because it will be a breaking change in the next major version release. Instead,
  * include a header above the defintion list component that uses a SKY UX supported class:
  * `<h3 class="sky-subsection-heading sky-margin-stacked-compact">My list</h3>`
  * `<sky-definition-list>...</sky-definition-list>`
