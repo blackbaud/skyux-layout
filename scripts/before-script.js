7ab52f2480f31fe9faf2bf81046bdd065df530cb
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-04-01 10:10:17 -0400

    Add hook

---
+++ scripts/before-script.js
@@ -0,0 +1,17 @@
+const crossSpawn = require('cross-spawn');
+
+async function runAsync() {
+  crossSpawn.sync('npx', [
+    '-p', '@skyux-sdk/cli',
+    'skyux', 'build-public-library',
+    '--logFormat', 'none'
+  ], {
+    stdio: 'inherit'
+  });
+
+  require('./dev-bundle');
+}
+
+module.exports = {
+  runAsync
+};
