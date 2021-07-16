b726f70f76aee51228e376800df5f65f5a4d8c23
Trevor Burch <Trevor.Burch@blackbaud.com>
2021-07-16 12:47:14 -0400

    Clarify that `sky-row` now should be used within a `sky-fluid-grid` component.

--- src/app/public/modules/fluid-grid/row.component.ts
+++ src/app/public/modules/fluid-grid/row.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 
+/**
+ * A previous implementation of the fluid grid allowed for the `sky-row` component to be used
+ * without a wrapping `sky-fluid-grid` component; however, this is no longer officially supported.
+ */
 @Component({
   selector: 'sky-row',
   templateUrl: './row.component.html',
