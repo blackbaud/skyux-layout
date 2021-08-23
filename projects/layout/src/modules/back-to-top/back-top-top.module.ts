5f31dcf50b56e7a8cbdb85d656c1cfc51ee404b2
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-08-23 15:26:11 -0400

    Eject

---
+++ projects/layout/src/modules/back-to-top/back-top-top.module.ts
@@ -0,0 +1,48 @@
+import {
+  NgModule
+} from '@angular/core';
+
+import {
+  CommonModule
+} from '@angular/common';
+
+import {
+  SkyDockModule
+} from '@skyux/core';
+
+import {
+  SkyI18nModule
+} from '@skyux/i18n';
+
+import {
+  SkyLayoutResourcesModule
+} from '../shared/sky-layout-resources.module';
+
+import {
+  SkyBackToTopDirective
+} from './back-to-top.directive';
+
+import {
+  SkyBackToTopComponent
+} from './back-to-top.component';
+
+@NgModule({
+  declarations: [
+    SkyBackToTopComponent,
+    SkyBackToTopDirective
+  ],
+  imports: [
+    CommonModule,
+    SkyDockModule,
+    SkyI18nModule,
+    SkyLayoutResourcesModule
+  ],
+  exports: [
+    SkyBackToTopComponent,
+    SkyBackToTopDirective
+  ],
+  entryComponents: [
+    SkyBackToTopComponent
+  ]
+})
+export class SkyBackToTopModule { }
