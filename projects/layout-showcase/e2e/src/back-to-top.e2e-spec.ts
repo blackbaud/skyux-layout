3b5083d7f732ef67d8ca804d3483be064b329107
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-09-13 11:32:29 -0400

    Fix back-to-top visual tests

--- projects/layout-showcase/e2e/src/back-to-top.e2e-spec.ts
+++ projects/layout-showcase/e2e/src/back-to-top.e2e-spec.ts
@@ -3,28 +3,34 @@ import {
   SkyHostBrowser
 } from '@skyux-sdk/e2e';
 
-// Disabled temporarily to allow SKY UX 5 release.
-xdescribe('Back to top', () => {
+describe('Back to top', () => {
+  beforeEach(async () => {
+    await SkyHostBrowser.setWindowBreakpoint('xs');
+  });
+
   it('should match previous screenshot', async (done) => {
     await SkyHostBrowser.get('visual/back-to-top');
-    await SkyHostBrowser.scrollTo('p:last-child');
-    expect('#screenshot-window').toMatchBaselineScreenshot(done, {
+    await SkyHostBrowser.scrollTo('.scroll-target');
+
+    expect('.app-screenshot-window').toMatchBaselineScreenshot(done, {
       screenshotName: 'infinite-scroll-back-to-top'
     });
   });
 
   it('should match previous screenshot with scrollable parent element', async (done) => {
     await SkyHostBrowser.get('visual/back-to-top-scrollable-parent');
-    await SkyHostBrowser.scrollTo('p:last-child');
-    expect('#screenshot-window').toMatchBaselineScreenshot(done, {
+    await SkyHostBrowser.scrollTo('.scroll-target');
+
+    expect('.app-screenshot-window').toMatchBaselineScreenshot(done, {
       screenshotName: 'infinite-scroll-back-to-top-scrollable-parent'
     });
   });
 
   it('should match previous screenshot with message stream and no button', async (done) => {
     await SkyHostBrowser.get('visual/back-to-top-message-stream');
-    await SkyHostBrowser.scrollTo('p:last-child');
-    expect('#screenshot-window').toMatchBaselineScreenshot(done, {
+    await SkyHostBrowser.scrollTo('.scroll-target');
+
+    expect('.app-screenshot-window').toMatchBaselineScreenshot(done, {
       screenshotName: 'back-to-top-message-stream'
     });
   });
