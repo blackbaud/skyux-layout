edee33541c4e2e054188e8e48b521474f7aff203
Alex Kingman <alex.kingman@blackbaud.me>
2021-12-15 13:55:23 -0500

    added padding to controls

--- projects/layout/src/modules/box/box.component.ts
+++ projects/layout/src/modules/box/box.component.ts
@@ -20,14 +20,16 @@ export class SkyBoxComponent {
 
   /**
    * Specifies the HTML element ID (without the leading `#`) of the element that labels
-   * the box. This sets the box's `aria-labelledby` attribute [to support accessibility](https://developer.blackbaud.com/skyux/learn/accessibility).
+   * the box. This sets the box's `aria-labelledby` attribute
+   * [to support accessibility](https://developer.blackbaud.com/skyux/learn/accessibility).
    * If the box does not include a visible label, use `ariaLabel` instead.
    */
   @Input()
   public ariaLabelledBy: string;
 
   /**
-   * Specifies an ARIA role for the box [to support accessibility](https://developer.blackbaud.com/skyux/learn/accessibility)
+   * Specifies an ARIA role for the box
+   * [to support accessibility](https://developer.blackbaud.com/skyux/learn/accessibility)
    * by indicating what the box contains. For information about
    * how an ARIA role indicates what an item represents on a web page,
    * see the [WAI-ARIA roles model](https://www.w3.org/WAI/PF/aria/roles).
